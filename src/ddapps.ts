b521f83457b710b4b5565dc84eea0a19ff387184
soootaleb <talebs@icloud.com>
2022-11-16 15:27:51 +0100

    ddapps:1.6.1

--- src/ddapps.ts
+++ src/ddapps.ts
@@ -27,7 +27,7 @@ export class DDAPPS<
 
   public static readonly PRODUCT = {
     ...Deno.version,
-    ddapps: "1.6.0",
+    ddapps: "1.6.1",
   };
 
   protected boot = ["log", "api", "mon", "peer", "net"];
